44656119428802b863a753f1faeb69cd43d15f50
jeygen <83786292+jeygen@users.noreply.github.com>
2022-01-14 15:04:15 -0500

    Update FlightTest.java

--- flight-booking-system/src/test/java/com/coe528/FlightTest.java
+++ flight-booking-system/src/test/java/com/coe528/FlightTest.java
@@ -1,25 +1,31 @@
 package com.coe528;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class FlightTest {
     
     @Test
     public void testConstructor() {
-        Flight instance = new Flight(10, "Toronto", "Toronto", "5:00 pm", 10, 45.4);
+        Flight instance = new Flight(10, "Toronto", "Edmonton", "5:00 pm", 10, 45.4);
+        /*
         if (instance instanceof Flight)
-            assertTrue("Valid Constructor");
+            assertTrue(true);
         else    
             fail("Invalid Constructor");
+        */
+        assertTrue(instance instanceof Flight);
+        // Could also check instance variables if correct
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = Exception.class) // Start here
     public void testInvalidConstructor() {
         try {
             Flight instance = new Flight(10, "Toronto", "Toronto", "5:00 pm", 10, 45.4);
             fail("Exception was expected");
         }
         catch (Exception e) {
-            assertTrue("Invalid Constructor");
+            //assertTrue(true);
         }
     }
 
