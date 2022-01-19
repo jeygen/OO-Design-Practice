451991e38a2344701fbd8786a9f1e5b2e43aaedd
jeygen <83786292+jeygen@users.noreply.github.com>
2022-01-19 10:02:46 -0500

    Modified Test class

--- flight-booking-system/src/test/java/com/coe528/FlightTest.java
+++ flight-booking-system/src/test/java/com/coe528/FlightTest.java
@@ -1,6 +1,5 @@
 package com.coe528;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class FlightTest {
@@ -8,12 +7,6 @@ public class FlightTest {
     @Test
     public void testConstructor() {
         Flight instance = new Flight(10, "Toronto", "Edmonton", "5:00 pm", 10, 45.4);
-        /*
-        if (instance instanceof Flight)
-            assertTrue(true);
-        else    
-            fail("Invalid Constructor");
-        */
         assertTrue(instance instanceof Flight);
         // Could also check instance variables if correct ?
     }
