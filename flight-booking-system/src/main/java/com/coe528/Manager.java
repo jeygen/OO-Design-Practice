7e244639afb72ac64b138238179ddcf889a24aef
jeygen <83786292+jeygen@users.noreply.github.com>
2022-01-19 09:46:00 -0500

    Cleaned-up Manager main()

--- flight-booking-system/src/main/java/com/coe528/Manager.java
+++ flight-booking-system/src/main/java/com/coe528/Manager.java
@@ -13,14 +13,18 @@ public class Manager {
         int numOfFlights;
         if (scan.hasNextInt()) {
             numOfFlights = scan.nextInt();
-            if (numOfFlights < 0)
+            if (numOfFlights <= 0)
                 throw new IllegalArgumentException("Number of flights cannot be 0 or less");
         }
         else {
+            throw new IllegalArgumentException("Number of flights must be an integer.");
+            /*
             numOfFlights = 0;
             System.out.println("No flights available at this time. Goodbye.");
             System.exit(0);
+            */
         }
+        
         fList = new Flight[numOfFlights];
         
         for (int i = 0; i < numOfFlights; i++) { // Prompts user to input values for Flight object and constructs Flight objects.
@@ -55,15 +59,15 @@ public class Manager {
     }
 
     public void displayAvailableFlights(String origin, String destination) { // Displays unbooked flights of specified origin, dest
+            System.out.println("Printing available flights with specified origin and destination: ");
         for(int i = 0; i < fList.length; i++) {
             if (fList[i].getCapacity() > 0 && fList[i].getOrigin().toUpperCase().equals(origin.toUpperCase()) && fList[i].getDestination().toUpperCase().equals(destination.toUpperCase())) {
-                System.out.println("Printing available flights with specified origin and destination: ");
                 System.out.println(fList[i].toString());
             }
-            else if (fList[i].getOrigin().toUpperCase().equals(origin.toUpperCase()) && fList[i].getDestination().equals(destination.toUpperCase()))  
+            else if (fList[i].getOrigin().toUpperCase().equals(origin.toUpperCase()) && fList[i].getDestination().toUpperCase().equals(destination.toUpperCase()))  
                 System.out.println(fList[i].getFlightNumber() + " is booked.");
             else 
-                System.out.println("No flights with that origin and destination exist");  
+                System.out.println("Flight " + fList[i].getFlightNumber() + " does not match the desired cities.");  
         }
     }
 
@@ -88,58 +92,71 @@ public class Manager {
     public void bookSeat(int flightNumber, Passenger p) { 
         double price = 0;
 
-        
-
         for(int i = 0; i < fList.length; i++) {
             if (fList[i].getFlightNumber() == flightNumber) {
                 fList[i].setCapacity(fList[i].getCapacity() - 1);
-               // price = p.applyDiscount(fList[i].getOriginalPrice());
              
-                if (p instanceof Member) { // Keep getting wrong values for price 
+                if (p instanceof Member) {  
                     price = ((Member)p).applyDiscount(fList[i].getOriginalPrice());
+                    System.out.println("Passenger with member status booked with ticket number : " + (Ticket.getNumber() + 1));
                 }
                 else {    
                     price = ((NonMember)p).applyDiscount(fList[i].getOriginalPrice());
+                    System.out.println("Passenger with nonmember status booked with ticket number : " + (Ticket.getNumber() + 1));
                 }                    
                 tickList[Ticket.getNumber()] = new Ticket(p, fList[i], price);
             }
-            else
-                System.out.println("No flight with that flight number exists.");
+            //else
+               // System.out.println("No flight with that flight number exists.");
         } 
     }
 
     public static void main(String [] args) {
+        
+        // Creating and populating flights through user input
         Manager manager = new Manager();
-        manager.createFlights(); // Creating and populating flights through user input
+        manager.createFlights(); 
 
-        manager.displayAvailableFlights("YYZ", "YEG"); // Seeing all availabe flights with input origin and destination
+        // Prints total number of tickets and print current capacity for each flight booked
+        System.out.println("Total number of tickets: " + Manager.tickList.length);
+        for (int i = 0; i < Manager.fList.length; i++)
+            System.out.println("Current flight capacity of " + Manager.fList[i].getFlightNumber() + " is " + Manager.fList[i].getCapacity());
+        System.out.println();
+
+        // Seeing all availabe flights with input origin and destination
+        manager.displayAvailableFlights("YYZ", "YEG"); 
         System.out.println(); 
         
-        Member mem1 = new Member("Josh", 20, 3); // Creating Member and non-Member Passenger objects
+         // Creating Member and non-Member Passenger objects
+        Member mem1 = new Member("Josh", 20, 3);        
         Member mem2 = new Member("Moe", 30, 10);
         NonMember nonMem1 = new NonMember("Xi", 72);
         NonMember nonMem2 = new NonMember("Karen", 52);
         NonMember nonMem3 = new NonMember("Kai", 10);
 
-        // Booking Passengeris on Flight 737
+        // Booking Passengers on Flight 737 to max capacity
         manager.bookSeat(737, mem1);  
         manager.bookSeat(737, mem2);
         manager.bookSeat(737, nonMem1); 
         manager.bookSeat(737, nonMem2);
         manager.bookSeat(737, nonMem3);
-        //System.out.println(manager.getFlight(737).getOriginalPrice());
-        System.out.println("Total number of tickets issued: " + Manager.tickList.length);
-        System.out.println("Current flight capacity of " + Manager.fList[0].getFlightNumber() + " is " + Manager.fList[0].getCapacity());
+        System.out.println(); 
+        
+        // Print current capacity for each flight booked
+        for (int i = 0; i < Manager.fList.length; i++)
+            System.out.println("Current flight capacity of " + Manager.fList[i].getFlightNumber() + " is " + Manager.fList[i].getCapacity());
         System.out.println();
 
-
-        System.out.println("Printing out all issued tickets:"); // Prints out all issued tickets
+        // Prints out all issued tickets
+        System.out.println("Printing out all issued tickets:"); 
         for(int i = 0; i < Manager.tickList.length; i++) {
             if (Manager.tickList[i] != null)
-                System.out.println("Ticket: " +  Ticket.getNumber() + " ---> " + Manager.tickList[i]);
+                System.out.println("Ticket" + " ---> " + Manager.tickList[i]);
         }
+        System.out.println();
 
-        manager.displayAvailableFlights("YYZ", "YEG"); // Seeing all availabe flights with input origin and destination
+        // Checking all available flights again
+        manager.displayAvailableFlights("YYZ", "YEG");
 
         
     }
