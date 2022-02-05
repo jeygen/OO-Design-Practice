ef5fccf27ed85ab96e42dd2769ec8205434be074
jeygen <83786292+jeygen@users.noreply.github.com>
2022-02-05 18:42:24 -0500

    Update QueueOfDistinctStrings.java

--- abs_func_rep_invar/src/main/java/com/coe528/coe528/QueueOfDistinctStrings.java
+++ abs_func_rep_invar/src/main/java/com/coe528/coe528/QueueOfDistinctStrings.java
@@ -10,15 +10,20 @@ public class QueueOfDistinctStrings {
  // The abstraction function is:
  // a) Write the abstraction function here
  // 
- // 
- //
+ //     AF(q) = An abstract queue of distinct strings where q is a QueueOfDistinctStrings object.  
+ //             Where q.list.get(i) == a distinct string element, i is an integer index for the q.list.eue with a size q.list.[1..n].
+ // --------- Do I need any of this additional stuff?
+ //             Where q.list.get(0) == the front of the q.list.eue.
+ //             Where q.list.get((q.list.size()-1)) == the end of the q.list.eue.
+ //             Where q.list.dequeue() removes the an element from the front of the q.list.eue.
+ //             Where q.list.enqueue() adds a new element to the end of the q.list.eue.
  //
  // The rep invariant is:
  // b) Write the rep invariant here
- // 
- // 
- // 
- //
+ //     
+ //     RI(q) = True if all elements are unique.
+ //             False otherwise.
+ // ---------- Should this be code?
  
     //the rep
     private ArrayList<String> items;
@@ -50,21 +55,65 @@ public class QueueOfDistinctStrings {
     // object; otherwise returns false 
     public boolean repOK() {
     // c) Write the code for the repOK() here
-        boolean placeholderImade = false;
-        return false;
-    }
+    /*
+        int count = 0;
+        String temp = new String("placeholder");
+        for (int i = 0; i < this.items.size() ; i++) {
+            temp = this.items.get(i);
+                for (int j = 0; j < this.items.size() ; j++) {
+                    if (temp.equals(this.items.get(j))) 
+                        count++;
+                }
+            }
+                        
+        if (count > 3)
+            return false;
+            */
+        ArrayList<String> compList = new ArrayList<String>();
+        
+        for (String s : this.items) {
+            if (!compList.contains(s)) {
+                compList.add(s);
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < this.items.size() ; i++) {
+            if (!this.items.get(i).equals(compList.get(i)))
+                //count++;    
+                System.out.println(count);
+                return false;
+        }
+
+        return true;
+        }
     
     // EFFECTS: Returns a string that contains the strings in the 
     // queue, the front element and the end element. 
     // Implements the abstraction function. 
     public String toString() {
     // d) Write the code for the toString() here
-        String placeholderImade = "placeholder";
-        return placeholderImade;
+
+        // should this be every element or just front and end?
+        return "The front elements is: " + this.items.get(0) + " and the end element is: " + this.items.get(this.items.size() - 1);
     }
     
+    // I added this to test stuff // delete before submission
+    public static void main(String [] args) throws Exception {
+        QueueOfDistinctStrings q = new QueueOfDistinctStrings();
+        String o = new String("one");
+        q.enqueue(o);
+        q.enqueue("two");
+        q.enqueue("three");
+        System.out.println(q);
+        q.dequeue();
+        System.out.println(q);
+        System.out.println(q.repOK());
+        System.out.println(q.repOK());
+        
 
-    public static void main(String [] args) {
+
+
         System.out.println("Hello Borld");
+        System.out.println(q);
     }
 }
