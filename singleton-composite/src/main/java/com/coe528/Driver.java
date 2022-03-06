4312a8fb0a73fa3dc7ac6daa6c2a2f0c8567d3b7
jeygen <83786292+jeygen@users.noreply.github.com>
2022-03-06 11:32:17 -0500

    Cleaned-up and ready to submit

--- singleton-composite/src/main/java/com/coe528/Driver.java
+++ singleton-composite/src/main/java/com/coe528/Driver.java
@@ -12,21 +12,17 @@ public class Driver {
         FoodComponent fc1 = new FoodCategory("frozen");
         FoodComponent fc2 = new FoodCategory("meat");
         FoodComponent fc3 = new FoodCategory("vegetables");
-        ((FoodCategory)fc1).add(fc2); // cat2 added to cat1, lvl of cat2 increases by 1
-        ((FoodCategory)fc1).add(fc3); // cat3 added to cat1, lvl of cat1 increases by 1
-        ((FoodCategory)fc1).add(fi6); // item6 added to cat1, lvl of item6 is (cat1.lvl + 1)
+        ((FoodCategory)fc1).add(fc2); 
+        ((FoodCategory)fc1).add(fc3); 
+        ((FoodCategory)fc1).add(fi6); 
         
-        ((FoodCategory)fc2).add(fi3); // item 3 added to cat2, lvl of item3 is (cat2.lvl + 1)
-        ((FoodCategory)fc2).add(fi4); // item 4 added to cat2, lvl of item4 is (cat2.lvl + 1)
+        ((FoodCategory)fc2).add(fi3); 
+        ((FoodCategory)fc2).add(fi4); 
         
-        ((FoodCategory)fc3).add(fi1); // item 1,2,5 are level (cat3.lvl + 1)
+        ((FoodCategory)fc3).add(fi1);
         ((FoodCategory)fc3).add(fi2);
         ((FoodCategory)fc3).add(fi5);
  
         fc1.print(0);
-        //fc2.print(0); // delete
-        //fc3.print(0); // delete
-        //fi1.print(0); // del before submitting
-        //System.out.println(((FoodItem)fi5).getLevel());
     }
 }
